fab9daa00334243b07321f6066149187271e0d3d
Rodrigo <rcoto@protonmail.com>
2018-09-17 11:24:19 -0700

    Minor adjustments before starting review part

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('watch', () => {
   require('./server.js');
   livereload.listen()
   gulp.watch(CSS_PATH, ['styles'])
-  gulp.watch(SCRIPTS_PATH, ['scripts', 'restaurant'])
+  gulp.watch(SCRIPTS_PATH, ['scripts'])
 });
 
 // =================================================
