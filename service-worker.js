a9035a2b47ff77363e5f8acaf86241e3298b5d46
Rodrigo <rcoto@protonmail.com>
2019-07-15 01:58:02 -0700

    Fix: service worker to fetch to azure

--- service-worker.js
+++ service-worker.js
@@ -1,75 +1,75 @@
 // Rewrite of Service-Worker
-const staticCacheName = 'restaurant-Static-V1';
-const contentImgsCache = 'restaurant-content-imgs';
-const allCaches = [
-  staticCacheName,
-  contentImgsCache
-]
+const staticCacheName = "restaurant-Static-V1";
+const contentImgsCache = "restaurant-content-imgs";
+const allCaches = [staticCacheName, contentImgsCache];
 const cacheFiles = [
-  './',
-  './index.html',
-  './restaurant.html',
-  './dist/restaurants-min.js',
-  './dist/restaurant_info-min.js',
-  './dist/styles.css',
-  './js/lazysizes.min.js',
-  './dist/promiseDb-min.js',
-]
+  "./",
+  "./index.html",
+  "./restaurant.html",
+  "./dist/restaurants-min.js",
+  "./dist/restaurant_info-min.js",
+  "./dist/styles.css",
+  "./js/lazysizes.min.js",
+  "./dist/promiseDb-min.js"
+];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(staticCacheName)
-    .then((cache) => {
-      return cache.addAll(cacheFiles)
+    caches.open(staticCacheName).then(cache => {
+      return cache.addAll(cacheFiles);
     })
-  )
+  );
 });
 
-self.addEventListener('activate',(event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.filter((cacheName) => {
-          return cacheName.startsWith('restaurant') && !allCaches.includes(cacheName);
-        })
-        .map(function (cacheName) {
-          return caches.delete(cacheName);
-        })
-      )
+        cacheNames
+          .filter(cacheName => {
+            return (
+              cacheName.startsWith("restaurant") &&
+              !allCaches.includes(cacheName)
+            );
+          })
+          .map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+      );
     })
-  )
+  );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   const requestUrl = new URL(event.request.url);
   // this Fetch request are already being handle by IDB
-  if(event.request.url.startsWith('http://localhost:1337')) {
+  if (event.request.url.startsWith("https://udacitysails.azurewebsites.net")) {
     return;
   }
 
-  if (requestUrl.pathname.startsWith('/img/')) {
-    event.respondWith(servePhoto(event.request))
+  if (requestUrl.pathname.startsWith("/img/")) {
+    event.respondWith(servePhoto(event.request));
     return;
   }
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       if (response) return response;
       return fetch(event.request);
     })
-  )
+  );
 });
 
 function servePhoto(request) {
-  const storageUrl = request.url
+  const storageUrl = request.url;
 
-  return caches.open(contentImgsCache).then((cache) => {
-    return cache.match(storageUrl).then((response) => {
+  return caches.open(contentImgsCache).then(cache => {
+    return cache.match(storageUrl).then(response => {
       if (response) return response;
 
-      return fetch(request).then((networkResponse) => {
+      return fetch(request).then(networkResponse => {
         cache.put(storageUrl, networkResponse.clone());
-        return networkResponse
-      })
-    })
-  })
+        return networkResponse;
+      });
+    });
+  });
 }
